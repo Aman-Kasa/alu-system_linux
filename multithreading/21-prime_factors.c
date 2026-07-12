0f9c39046ae23051081116b500c2625da90b5723
Aman-Kasa <a.kasa@alustudent.com>
2026-07-12 18:13:08 +0200

    lib added

--- multithreading/21-prime_factors.c
+++ multithreading/21-prime_factors.c
@@ -1,97 +1,72 @@
 #include "multithreading.h"
-#include <stdlib.h>
-#include <string.h>
+
+
 
 /**
- * lim_binary - finds the integer square root of a number
- * @n: the number to compute the square root of
- *
- * Return: the integer square root of n
- */
-static unsigned long lim_binary(unsigned long n)
-{
-	unsigned long low = 0, high = n, mid;
-
-	while (low <= high)
-	{
-		mid = (low + high) / 2;
-		if (mid * mid <= n)
-			low = mid + 1;
-		else
-			high = mid - 1;
-	}
-	return (high);
-}
-
-/**
- * prime_factors - computes the prime factors of a number given as a string
- * @s: string representing the number to factorize
- *
- * Return: pointer to a list_t containing the prime factors, or NULL on failure
- */
+* prime_factors - func
+* @s: char const *
+* Return: lsit_t *
+*/
 list_t *prime_factors(char const *s)
 {
-	list_t *list;
-	unsigned long n, i, lim;
+	unsigned long int n = 0, lim = 0, i = 2, *p = NULL;
+	list_t *list = NULL;
 
-	if (!s)
-		return (NULL);
-
-	list = malloc(sizeof(*list));
-	if (!list)
-		return (NULL);
+	list = malloc(sizeof(list_t));
 	list_init(list);
-
 	n = strtoul(s, NULL, 10);
-
-	/* handle factor 2 separately to optimize */
-	while (n % 2 == 0)
+	if (n < 2)
 	{
-		unsigned long *factor = malloc(sizeof(*factor));
-
-		if (!factor || list_add(list, factor) == NULL)
-		{
-			list_destroy(list, free);
-			free(list);
-			return (NULL);
-		}
-		*factor = 2;
-		n /= 2;
+		p = malloc(sizeof(unsigned long int));
+		*p = n;
+		list_add(list, (void *)p);
 	}
-
-	/* check odd factors up to sqrt(n) */
-	lim = lim_binary(n);
-	for (i = 3; i <= lim; i += 2)
+	else
 	{
-		while (n % i == 0)
+		lim = lim_binary(n);
+		while (i <= lim)
 		{
-			unsigned long *factor = malloc(sizeof(*factor));
-
-			if (!factor || list_add(list, factor) == NULL)
+			if (!(n % i))
 			{
-				list_destroy(list, free);
-				free(list);
-				return (NULL);
+				p = malloc(sizeof(unsigned long int));
+				*p = i;
+				list_add(list, (void *)p);
+				n /= i;
+				lim = lim_binary(n);
 			}
-			*factor = i;
-			n /= i;
-			lim = lim_binary(n);
+			else
+				i += (i == 2) ? 1 : 2;
 		}
-	}
-
-	/* if n is still greater than 1, it is a prime factor */
-	if (n > 1)
-	{
-		unsigned long *factor = malloc(sizeof(*factor));
-
-		if (!factor || list_add(list, factor) == NULL)
+		if (n > 1)
 		{
-			list_destroy(list, free);
-			free(list);
-			return (NULL);
+			p = malloc(sizeof(unsigned long int));
+			*p = n;
+			list_add(list, (void *)p);
 		}
-		*factor = n;
 	}
-
 	return (list);
+}
+
+
+/**
+* lim_binary - func
+* @n: unsigned long int
+* Return: unsigned long int
+*/
+unsigned long int lim_binary(unsigned long int n)
+{
+	unsigned long int r = 0, end = n / 2, begin = 2, mid = (end + begin) / 2;
+
+	while (begin < end)
+	{
+		r = mid * mid;
+		if (r < n)
+			begin = mid;
+		else if (r > n)
+			end = mid - 1;
+		else
+			break;
+		mid = (end + begin) / 2 + 1;
+	}
+	return (mid);
 }
