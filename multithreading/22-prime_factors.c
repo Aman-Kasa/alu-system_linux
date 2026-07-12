cdff3c73ef2366157111eaed2ecc1a0f2d2721f6
Aman-Kasa <a.kasa@alustudent.com>
2026-07-12 17:58:51 +0200

    21 prime

--- multithreading/22-prime_factors.c
+++ multithreading/22-prime_factors.c
@@ -1,65 +1,89 @@
 #include "multithreading.h"
-
-
+#include <stdlib.h>
+#include <string.h>
 
 /**
-* create_task - func
-* @entry: task_entrey_t
-* @param: void *
-* Return: task_t *
-*/
+ * create_task - creates a task structure
+ * @entry: pointer to the task entry function
+ * @param: parameter to pass to the entry function
+ * Return: pointer to the created task, or NULL on failure
+ */
 task_t *create_task(task_entry_t entry, void *param)
 {
-	task_t *task = NULL;
+	task_t *task;
 
-	task = malloc(sizeof(task_t));
-	memset(task, 0, sizeof(task_t));
+	task = malloc(sizeof(*task));
+	if (!task)
+		return (NULL);
+	memset(task, 0, sizeof(*task));
 	task->entry = entry;
 	task->param = param;
 	task->status = PENDING;
-	task->result = NULL;
+	pthread_mutex_init(&task->lock, NULL);
 	return (task);
 }
 
-
 /**
-* destroy_task - func
-* @task: task_t
-*/
+ * destroy_task - destroys a task and its result
+ * @task: pointer to the task to destroy
+ */
 void destroy_task(task_t *task)
 {
-	list_destroy(task->result, free);
-	free(task->result);
+	if (!task)
+		return;
+	if (task->result)
+	{
+		list_destroy((list_t *)task->result, free);
+		free(task->result);
+	}
+	pthread_mutex_destroy(&task->lock);
 	free(task);
 }
 
-
 /**
-* exec_tasks - func
-* @tasks: list_t const *
-* Return: void *
-*/
+ * exec_tasks - thread entry function that executes a list of tasks
+ * @tasks: pointer to the list of tasks (list_t *)
+ * Return: NULL
+ */
 void *exec_tasks(list_t const *tasks)
 {
-	node_t *node = NULL;
-	task_t *task = NULL;
-	size_t size = 0, i = 0;
+	node_t *node;
+	size_t i = 0;
 
 	if (!tasks)
 		return (NULL);
+
 	node = tasks->head;
-	size = tasks->size;
-	while (node && (i < size))
+	while (node && i < tasks->size)
 	{
-		task = node->content;
+		task_t *task = (task_t *)node->content;
+
+		/* lock before checking status to avoid race */
+		pthread_mutex_lock(&task->lock);
 		if (task->status == PENDING)
 		{
-			pthread_mutex_lock(&task->lock);
 			task->status = STARTED;
-			tprintf("[%02d] Started\n", i);
+			pthread_mutex_unlock(&task->lock);
+
+			tprintf("[%lu] [%02lu] Started\n", pthread_self(), i);
+
 			task->result = task->entry(task->param);
-			tprintf("[%02d] Success\n", i);
-			task->status = SUCCESS;
+
+			pthread_mutex_lock(&task->lock);
+			if (task->result)
+			{
+				task->status = SUCCESS;
+				tprintf("[%lu] [%02lu] Success\n", pthread_self(), i);
+			}
+			else
+			{
+				task->status = FAILURE;
+				tprintf("[%lu] [%02lu] Failure\n", pthread_self(), i);
+			}
+			pthread_mutex_unlock(&task->lock);
+		}
+		else
+		{
 			pthread_mutex_unlock(&task->lock);
 		}
 		i++;
