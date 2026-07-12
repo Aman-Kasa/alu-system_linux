33bc394ceee7a377cd6fc07273c53f2da78338c2
Aman-Kasa <a.kasa@alustudent.com>
2026-07-12 20:12:19 +0200

    fix: resolve list.h struct redefinition by deferring to multithreading.h

--- multithreading/list.h
+++ multithreading/list.h
@@ -1,25 +1,6 @@
 #ifndef LIST_H
 #define LIST_H
 
-#include <stddef.h>
-
-typedef struct node_s
-{
-    void *content;
-    struct node_s *prev;
-    struct node_s *next;
-} node_t;
-
-typedef struct list_s
-{
-    node_t *head;
-    node_t *tail;
-    size_t size;
-} list_t;
-
-/* ADD THIS LINE HERE */
-typedef void (*node_func_t)(void *);
-
-/* Your list function prototypes here... */
+#include "multithreading.h"
 
 #endif /* LIST_H */
