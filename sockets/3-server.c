19ef67c33c4b46c717d18a640c3df19ffd16ee6c
Aman-Kasa <a.kasa@alustudent.com>
2026-07-13 18:25:56 +0200

    .gitignore added

--- sockets/3-server.c
+++ sockets/3-server.c
@@ -1,17 +1,88 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <sys/socket.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
-#include "socket_utils.h"
 
 #define PORT 12345
 #define BUFFER_SIZE 4096
 
 /**
- * main - opens an IPv4/TCP socket, binds it to port 12345 on any
- * address, and repeatedly accepts connections, printing the client's
- * IP address and any message it sends before closing the connection
+ * create_server_socket - creates and prepares server socket
+ *
+ * Return: socket file descriptor
+ */
+int create_server_socket(void)
+{
+	int sockfd;
+	int opt;
+	struct sockaddr_in addr;
+
+	sockfd = socket(AF_INET, SOCK_STREAM, 0);
+	if (sockfd == -1)
+	{
+		perror("socket");
+		exit(EXIT_FAILURE);
+	}
+
+	opt = 1;
+	if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR,
+		       &opt, sizeof(opt)) == -1)
+	{
+		perror("setsockopt");
+		exit(EXIT_FAILURE);
+	}
+
+	memset(&addr, 0, sizeof(addr));
+	addr.sin_family = AF_INET;
+	addr.sin_addr.s_addr = INADDR_ANY;
+	addr.sin_port = htons(PORT);
+
+	if (bind(sockfd, (struct sockaddr *)&addr,
+		 sizeof(addr)) == -1)
+	{
+		perror("bind");
+		exit(EXIT_FAILURE);
+	}
+
+	if (listen(sockfd, 128) == -1)
+	{
+		perror("listen");
+		exit(EXIT_FAILURE);
+	}
+
+	return (sockfd);
+}
+
+/**
+ * handle_client - accepts and reads client messages
+ * @client_fd: client socket descriptor
+ * @client_addr: client address
+ */
+void handle_client(int client_fd, struct sockaddr_in *client_addr)
+{
+	char buffer[BUFFER_SIZE];
+	ssize_t bytes_read;
+
+	printf("Client connected: %s\n",
+	       inet_ntoa(client_addr->sin_addr));
+
+	bytes_read = recv(client_fd, buffer,
+			  sizeof(buffer) - 1, 0);
+
+	if (bytes_read > 0)
+	{
+		buffer[bytes_read] = '\0';
+		printf("%s\n", buffer);
+	}
+
+	close(client_fd);
+}
+
+/**
+ * main - starts server and handles connections
  *
  * Return: always 0
  */
@@ -21,31 +92,23 @@ int main(void)
 	int client_fd;
 	struct sockaddr_in client_addr;
 	socklen_t client_len;
-	char buffer[BUFFER_SIZE];
-	ssize_t bytes_read;
 
-	sockfd = create_server_socket(PORT);
+	sockfd = create_server_socket();
+
 	printf("Server listening on port %d\n", PORT);
 
 	while (1)
 	{
 		client_len = sizeof(client_addr);
-		client_fd = accept(sockfd, (struct sockaddr *)&client_addr,
-				    &client_len);
+
+		client_fd = accept(sockfd,
+				   (struct sockaddr *)&client_addr,
+				   &client_len);
+
 		if (client_fd == -1)
 			continue;
 
-		printf("Client connected: %s\n",
-		       inet_ntoa(client_addr.sin_addr));
-
-		bytes_read = recv(client_fd, buffer, sizeof(buffer) - 1, 0);
-		if (bytes_read > 0)
-		{
-			buffer[bytes_read] = '\0';
-			printf("Message received: \"%s\"\n", buffer);
-		}
-
-		close(client_fd);
+		handle_client(client_fd, &client_addr);
 	}
 
 	return (0);
