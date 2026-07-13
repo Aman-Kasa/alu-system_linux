c9f4dfee07f226e3db35dd929f60fe726afefb10
Aman-Kasa <a.kasa@alustudent.com>
2026-07-13 18:12:17 +0200

    task1 fixedj

--- sockets/1-server.c
+++ sockets/1-server.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include <unistd.h>
+#include <sys/socket.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
-#include "socket_utils.h"
 
 #define PORT 12345
 
@@ -16,11 +18,44 @@
 int main(void)
 {
 	int sockfd;
+	int opt;
 	int client_fd;
+	struct sockaddr_in addr;
 	struct sockaddr_in client_addr;
 	socklen_t client_len;
 
-	sockfd = create_server_socket(PORT);
+	sockfd = socket(AF_INET, SOCK_STREAM, 0);
+	if (sockfd == -1)
+	{
+		perror("socket");
+		exit(EXIT_FAILURE);
+	}
+
+	opt = 1;
+	if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR,
+		       &opt, sizeof(opt)) == -1)
+	{
+		perror("setsockopt");
+		exit(EXIT_FAILURE);
+	}
+
+	memset(&addr, 0, sizeof(addr));
+	addr.sin_family = AF_INET;
+	addr.sin_addr.s_addr = INADDR_ANY;
+	addr.sin_port = htons(PORT);
+
+	if (bind(sockfd, (struct sockaddr *)&addr, sizeof(addr)) == -1)
+	{
+		perror("bind");
+		exit(EXIT_FAILURE);
+	}
+
+	if (listen(sockfd, 128) == -1)
+	{
+		perror("listen");
+		exit(EXIT_FAILURE);
+	}
+
 	printf("Server listening on port %d\n", PORT);
 
 	while (1)
